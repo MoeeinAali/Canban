b901081f9640ecd09db2dd14a9a3fbd4c2aa9f34
Moeein Aali <moeeeinaali@gmail.com>
2026-02-06 12:05:42 +0330

    [FIX] Canban --> Kanban

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -7,5 +7,5 @@ import styles from "./Logo.module.css";
 export default function Logo(): ReactNode {
   const isCollapsed = useSidebarStore((state) => state.isCollapsed);
 
-  return <div className={styles.logo}>{isCollapsed ? "C" : "Canban"}</div>;
+  return <div className={styles.logo}>{isCollapsed ? "K" : "Kanban Board"}</div>;
 }
